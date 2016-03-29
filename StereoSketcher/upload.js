dc594e460a74a4bcbdf018acd877f8262ee12fb1
kanderson <KRAATW2008@yahoo.com>
2016-03-28 21:34:14 -0500

    made watermark 3d
    fixes #34

--- StereoSketcher/upload.js
+++ StereoSketcher/upload.js
@@ -56,12 +56,19 @@ function addWatermark() {
 	var size = 15
 	var height = 10;
 	var width = 145;
+	var destinationDot = calculateWatermarkDot();
 	var watermark = document.createElementNS("http://www.w3.org/2000/svg", "text");
 	watermark.setAttribute("id", "waterMark");
 	watermark.setAttribute("y", window.innerHeight-height);
-	watermark.setAttribute("x", window.innerWidth-width);
+	watermark.setAttribute("x", destinationDot.getAttribute("cx")-width);
 	watermark.setAttribute("font-size",size);
 	watermark.setAttribute("font-family","Arial");
+	var watermarkClone = document.createElementNS("http://www.w3.org/2000/svg", "text");
+	watermarkClone.setAttribute("id", "waterMarkClone");
+	watermarkClone.setAttribute("y", window.innerHeight-height);
+	watermarkClone.setAttribute("x", parseFloat(destinationDot.getAttribute("cx"))+destinationDot.getShift()*shiftSpeed+IPD-width);
+	watermarkClone.setAttribute("font-size",size);
+	watermarkClone.setAttribute("font-family","Arial");
 	var backgroundColor = parseInt("0x"+getBackgroundColor().substr(1,6));
 	var chunk1 = backgroundColor&0xff;
 	var chunk2 = (backgroundColor&0xff00)>>8;
@@ -69,14 +76,36 @@ function addWatermark() {
 	var backgroundSum = chunk1+chunk2+chunk3;
 	if(backgroundSum>382) {
 		watermark.setAttribute("fill", "black");
+		watermarkClone.setAttribute("fill", "black");
 	} else {
 		watermark.setAttribute("fill", "white");
+		watermarkClone.setAttribute("fill", "white");
 	}
 	watermark.textContent="StereoSketcher.com";
+	watermarkClone.textContent="StereoSketcher.com";
 	svg.appendChild(watermark);
+	svg.appendChild(watermarkClone);
+}
+
+function calculateWatermarkDot() {
+	var dots = getDots();
+	var targetDot = dots[0];
+	for(var ii=1;ii<dots.length;ii++) {
+		if(dots[ii].getShift()<targetDot.getShift()) {
+			targetDot = dots[ii];
+		}
+	}
+	for(var jj=0;jj<dots.length;jj++) {
+		if(dots[jj].getShift() == targetDot.getShift() && dots[jj].getAttribute("cx")>targetDot.getAttribute("cx")) {
+			targetDot = dots[jj];
+		}
+	}
+	return targetDot;
 }
 
 function hideWatermark() {
 	var watermark = document.getElementById("waterMark");
 	svg.removeChild(watermark);
+	var watermarkClone = document.getElementById("waterMarkClone");
+	svg.removeChild(watermarkClone);
 }

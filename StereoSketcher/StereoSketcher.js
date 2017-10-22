5b4c06e0f3c40adc604fb1d69fa17a3ec7c1d00f
Kevin Anderson <kraatw2008@yahoo.com>
2017-10-21 22:58:48 -0500

    adding workaround for Firefox's context menu that has become unblockable

--- StereoSketcher/StereoSketcher.js
+++ StereoSketcher/StereoSketcher.js
@@ -10,206 +10,210 @@ var loading=false;
 var middleMouseButtonLock=false;
 
 window.onload=function() {
-    svg = document.getElementById("svg");
-    dotGroup = document.getElementById("dots");
-    labelGroup = document.getElementById("labels");
-    shapeGroup = document.getElementById("shapes");
-    defs = document.getElementById("defs");
-    picker = document.getElementById('colorPicker');
-    zoomLabel = document.getElementById('zoomLabel');
-    toolGroup = document.getElementById('toolGroup');
-    fileInput = document.getElementById('fileInput');
-    imageDragger = document.getElementById('imageDrag');
-    baseDragger = document.getElementById('baseDrag');
-    imageButton = document.getElementById('imageButton');
-    baseButton = document.getElementById('baseButton');
-    gifFrames = document.getElementById('gifFrames');
-    blocker = document.getElementById("blocker");
-    loadingDiv = document.getElementById("loading");
-    configurationPopup = document.getElementById("configurationPopup");
-    framesInput = document.getElementById("framesInput");
-    frameTimeInput = document.getElementById("frameTimeInput");
-    shiftSpeedInput = document.getElementById("shiftSpeedInput");
-    axisVisibleInput = document.getElementById("axisVisibleInput");
+    document.addEventListener("contextmenu", function(e){
+        e.preventDefault();
+    }, false);
 
-    svg.onmousedown = function(event) {
-        pressX = event.clientX;
-        pressY = event.clientY;
-        prevX = pressX;
-        prevY = pressY;
-        if(event.button==0) {
-            editMode();
-            svg.onmousemove = function(event) {
-                preventDefault(event);
-                changeSelectangle(event);
-            };
-        }
-        if(event.button==1) {
-            if(!middleMouseButtonLock) {
-                var withExtrude = !!event.ctrlKey;
-                copyAndPasteSelectedShapes(event.clientX,event.clientY,withExtrude);
-                middleMouseButtonLock = true;
-                setTimeout(function() {
-                    middleMouseButtonLock = false;
-                },200);
-            }
-        }
-        if(event.button==2) {
-            editMode();
-            var dots = getDots();
-            svg.onmousemove = function(event) {
-                preventDefault(event);
-                if (!event.ctrlKey) {
-                    snapDots(dots, false, event, 0);
-                }
-            };
-        }
-    };
-    svg.onmouseup = function(event) {
-        if(wasAClick(event)) {
-            if(event.button==0) {
-                if(!event.shiftKey)
-                {
-                    deselectAll();
-                }
-            }
-            if(event.button==2) {
-                var dot=shapeFactory.createDot(event.clientX,event.clientY);
-                if(event.shiftKey) {
-                    dot.select();
-                    if (event.ctrlKey) {
-                        var previousDot = dot;
-                        createLinePressed(event);
-                        previousDot.select();
-                    }
-                }
-            }
-        } else if(event.button==0) {
-            releaseSelectangle(event);
-        }
-        stopDots();
-    };
-    svg.addEventListener("mousewheel",zoom,false);
-    svg.addEventListener("DOMMouseScroll",zoom,false);
+	svg = document.getElementById("svg");
+	dotGroup = document.getElementById("dots");
+	labelGroup = document.getElementById("labels");
+	shapeGroup = document.getElementById("shapes");
+	defs = document.getElementById("defs");
+	picker = document.getElementById('colorPicker');
+	zoomLabel = document.getElementById('zoomLabel');
+	toolGroup = document.getElementById('toolGroup');
+	fileInput = document.getElementById('fileInput');
+	imageDragger = document.getElementById('imageDrag');
+	baseDragger = document.getElementById('baseDrag');
+	imageButton = document.getElementById('imageButton');
+	baseButton = document.getElementById('baseButton');
+	gifFrames = document.getElementById('gifFrames');
+	blocker = document.getElementById("blocker");
+	loadingDiv = document.getElementById("loading");
+	configurationPopup = document.getElementById("configurationPopup");
+	framesInput = document.getElementById("framesInput");
+	frameTimeInput = document.getElementById("frameTimeInput");
+	shiftSpeedInput = document.getElementById("shiftSpeedInput");
+	axisVisibleInput = document.getElementById("axisVisibleInput");
 
-    imageDragger.addEventListener("change", imageDragHandler, false);
-    imageDragger.addEventListener("drop", imageDragHandler, false);
-    imageDragger.addEventListener("dragleave", dragHover, false);
-    imageDragger.addEventListener("dragover", dragHover, false);
-    imageDragger.addEventListener("mouseenter",imageHover,false);
-    imageDragger.addEventListener("mouseleave",imageLeave,false);
-    imageButton.addEventListener('change',imageButtonClick, false);
+	svg.onmousedown = function(event) {
+		pressX = event.clientX;
+		pressY = event.clientY;
+		prevX = pressX;
+		prevY = pressY;
+		if(event.button==0) {
+			editMode();
+			svg.onmousemove = function(event) {
+				preventDefault(event);
+				changeSelectangle(event);
+			};
+		}
+		if(event.button==1) {
+			if(!middleMouseButtonLock) {
+				var withExtrude = !!event.ctrlKey;
+				copyAndPasteSelectedShapes(event.clientX,event.clientY,withExtrude);
+				middleMouseButtonLock = true;
+				setTimeout(function() {
+					middleMouseButtonLock = false;
+				},200);
+			}
+		}
+		if(event.button==2) {
+			editMode();
+			var dots = getDots();
+			svg.onmousemove = function(event) {
+			    preventDefault(event);
+			    if (!event.ctrlKey) {
+			        snapDots(dots, false, event, 0);
+			    }
+			};
+		}
+	};
+	svg.onmouseup = function(event) {
+		if(wasAClick(event)) {
+			if(event.button==0) {
+				if(!event.shiftKey)
+				{
+					deselectAll();
+				}
+			}
+			if(event.button==2) {
+				var dot=shapeFactory.createDot(event.clientX,event.clientY);
+				if(event.shiftKey | event.altKey) {
+					dot.select();
+					if (event.ctrlKey) {
+						var previousDot = dot;
+						createLinePressed(event);
+						previousDot.select();
+					}
+				}
+			}
+		} else if(event.button==0) {
+			releaseSelectangle(event);
+		}
+		stopDots();
+	};
+	svg.addEventListener("mousewheel",zoom,false);
+	svg.addEventListener("DOMMouseScroll",zoom,false);
 
-    baseDragger.addEventListener("change", baseDragHandler, false);
-    baseDragger.addEventListener("drop", baseDragHandler, false);
-    baseDragger.addEventListener("dragleave", dragHover, false);
-    baseDragger.addEventListener("dragover", dragHover, false);
-    baseDragger.addEventListener("mouseenter",imageHover,false);
-    baseDragger.addEventListener("mouseleave",imageLeave,false);
-    baseButton.addEventListener('change',baseButtonClick,false);
+	imageDragger.addEventListener("change", imageDragHandler, false);
+	imageDragger.addEventListener("drop", imageDragHandler, false);
+	imageDragger.addEventListener("dragleave", dragHover, false);
+	imageDragger.addEventListener("dragover", dragHover, false);
+	imageDragger.addEventListener("mouseenter",imageHover,false);
+	imageDragger.addEventListener("mouseleave",imageLeave,false);
+	imageButton.addEventListener('change',imageButtonClick, false);
 
-    crossEyeMode();
-    picker.color.fromString("#000000");
-    zoomLabel.innerHTML = zoomLevel;
-    buildToolBar();
-    fileInput.addEventListener('change', function() {
-        load();
+	baseDragger.addEventListener("change", baseDragHandler, false);
+	baseDragger.addEventListener("drop", baseDragHandler, false);
+	baseDragger.addEventListener("dragleave", dragHover, false);
+	baseDragger.addEventListener("dragover", dragHover, false);
+	baseDragger.addEventListener("mouseenter",imageHover,false);
+	baseDragger.addEventListener("mouseleave",imageLeave,false);
+	baseButton.addEventListener('change',baseButtonClick,false);
+
+	crossEyeMode();
+	picker.color.fromString("#000000");
+	zoomLabel.innerHTML = zoomLevel;
+	buildToolBar();
+	fileInput.addEventListener('change', function() {
+		load();
     });
-    hideLoading();
-    setTimeout(function() {
-        window.onbeforeunload = function(e) {
-            preventDefault(e);
-        }
-    },5000);
+	hideLoading();
+	setTimeout(function() {
+		window.onbeforeunload = function(e) {
+			preventDefault(e);
+		}
+	},5000);
 };
 
 
 function wasAClick(event) {
-    return event.clientX == pressX && event.clientY == pressY;
+	return event.clientX == pressX && event.clientY == pressY;
 }
 
 function hideLoading() {
-    loading = false;
-    blocker.style.display = "none";
-    loadingDiv.style.display = "none";
+	loading = false;
+	blocker.style.display = "none";
+	loadingDiv.style.display = "none";
 }
 
 function showLoading(onlyMargin) {
-    loading = true;
-    blocker.style.display = "";
-    if(!onlyMargin) {
-        loadingDiv.style.display = "";
-    }
+	loading = true;
+	blocker.style.display = "";
+	if(!onlyMargin) {
+		loadingDiv.style.display = "";
+	}
 }
 
 document.addEventListener(
-    "keydown",
-    function(event)
-    {
-        if(configurationPopup.style.display == "none")
-        {
-            mapKeyPress(event);
-        }
-    },
-    false
+	"keydown",
+	function(event)
+	{
+		if(configurationPopup.style.display == "none")
+		{
+			mapKeyPress(event);
+		}
+	},
+	false
 );
 document.addEventListener("keyup", keyReleased, false);
 document.onclick = function(e) { if(e.button == 2 || e.button == 3) { e.preventDefault(); e.stopPropagation(); return(false); } };
 
 function getDots() {
-    return dotGroup.getElementsByClassName("dot");
+	return dotGroup.getElementsByClassName("dot");
 }
 
 function getLines() {
-    return shapeGroup.getElementsByClassName("line");
+	return shapeGroup.getElementsByClassName("line");
 }
 
 function getImages() {
-    return shapeGroup.getElementsByClassName("image");
+	return shapeGroup.getElementsByClassName("image");
 }
 
 function getBases() {
-    return shapeGroup.getElementsByClassName("base");
+	return shapeGroup.getElementsByClassName("base");
 }
 
 function getShapes() {
-    var item;
-    var items = shapeGroup.children;
-    var shapes = [];
-    for(var ii=0;ii<items.length;ii++) {
-        item = items[ii];
-        if(doesElementHaveClass(item,"line")||doesElementHaveClass(item,"face")||doesElementHaveClass(item,"image")||doesElementHaveClass(item,"base")) {
-            shapes.push(item);
-        }
-    }
-    return shapes;
+	var item;
+	var items = shapeGroup.children;
+	var shapes = [];
+	for(var ii=0;ii<items.length;ii++) {
+		item = items[ii];
+		if(doesElementHaveClass(item,"line")||doesElementHaveClass(item,"face")||doesElementHaveClass(item,"image")||doesElementHaveClass(item,"base")) {
+			shapes.push(item);
+		}
+	}
+	return shapes;
 }
 
 function getLinesAndFaces() {
-    var item;
-    var items = shapeGroup.children;
-    var shapes = [];
-    for(var ii=0;ii<items.length;ii++) {
-        item = items[ii];
-        if(doesElementHaveClass(item,"line")||doesElementHaveClass(item,"face")) {
-            shapes.push(item);
-        }
-    }
-    return shapes;
+	var item;
+	var items = shapeGroup.children;
+	var shapes = [];
+	for(var ii=0;ii<items.length;ii++) {
+		item = items[ii];
+		if(doesElementHaveClass(item,"line")||doesElementHaveClass(item,"face")) {
+			shapes.push(item);
+		}
+	}
+	return shapes;
 }
 
 function getFaces() {
-    return shapeGroup.getElementsByClassName("face");
+	return shapeGroup.getElementsByClassName("face");
 }
 
 function getSelected() {
-    return svg.getElementsByClassName("selected");
+	return svg.getElementsByClassName("selected");
 }
 
 function getLabels() {
-    return labelGroup.getElementsByClassName("label");
+	return labelGroup.getElementsByClassName("label");
 }
 
 function preventDefault(event) {
-    event.preventDefault ? event.preventDefault() : event.returnValue=false;
+	event.preventDefault ? event.preventDefault() : event.returnValue=false;
 }
--- StereoSketcher/selector.js
+++ StereoSketcher/selector.js
@@ -1,204 +1,204 @@
 'use strict';
 
 function lowlightAll() {
-    var highlit = shapeGroup.getElementsByClassName("highlit");
-    for (var ii=0;ii<highlit.length;ii++) {
-        highlit[ii].lowlight();
-    }
+	var highlit = shapeGroup.getElementsByClassName("highlit");
+	for (var ii=0;ii<highlit.length;ii++) {
+		highlit[ii].lowlight();
+	}
 }
 
 function selectDotsOfLine(line,event) {
-    line.dot1.select();
-    line.dot2.select();
+	line.dot1.select();
+	line.dot2.select();
 }
 
 function selectDotsOfFaceImageOrBase(shape,event) {
-    var dots = shape.dots;
-    for(var ii=0;ii<dots.length;ii++) {
-        dots[ii].select();
-    }
+	var dots = shape.dots;
+	for(var ii=0;ii<dots.length;ii++) {
+		dots[ii].select();
+	}
 }
 
 function selectShapesOfDot(dot,event) {
-    var lines = dot.lines;
-    var faces = dot.faces;
-    var images = dot.images;
-    var bases = dot.bases;
-    for(var ii=0;ii<lines.length;ii++) {
-        lines[ii].select();
-    }
-    for(var ii=0;ii<faces.length;ii++) {
-        faces[ii].select();
-    }
-    for(var ii=0;ii<images.length;ii++) {
-        images[ii].select();
-    }
-    for(var ii=0;ii<bases.length;ii++) {
-        bases[ii].select();
-    }
+	var lines = dot.lines;
+	var faces = dot.faces;
+	var images = dot.images;
+	var bases = dot.bases;
+	for(var ii=0;ii<lines.length;ii++) {
+		lines[ii].select();
+	}
+	for(var ii=0;ii<faces.length;ii++) {
+		faces[ii].select();
+	}
+	for(var ii=0;ii<images.length;ii++) {
+		images[ii].select();
+	}
+	for(var ii=0;ii<bases.length;ii++) {
+		bases[ii].select();
+	}
 }
 
 function selectAllContiguousShapes(sourceShape,event) {
-    var shapes = getShapes();
-    var shape;
-    if(!event.shiftKey) {
-        deselectAll();
-    } else {
-        for(var ii=0;ii<shapes.length;ii++) {
-            shape = shapes[ii];
-            if(shape.isSelected()) {
-                shape.deselect();
-                addClassToElement(shape,"tempSelect");
-            }
-        }
-    }
-    sourceShape.select();
-    recursiveSelectShapes(sourceShape);
-    for(var ii=0;ii<shapes.length;ii++) {
-        shape = shapes[ii];
-        if(doesElementHaveClass(shape,"tempSelect")) {
-            shape.select();
-            removeClassFromElement(shape,"tempSelect");
-        }
-    }
+	var shapes = getShapes();
+	var shape;
+	if(!event.shiftKey) {
+		deselectAll();
+	} else {
+		for(var ii=0;ii<shapes.length;ii++) {
+			shape = shapes[ii];
+			if(shape.isSelected()) {
+				shape.deselect();
+				addClassToElement(shape,"tempSelect");
+			}
+		}
+	}
+	sourceShape.select();
+	recursiveSelectShapes(sourceShape);
+	for(var ii=0;ii<shapes.length;ii++) {
+		shape = shapes[ii];
+		if(doesElementHaveClass(shape,"tempSelect")) {
+			shape.select();
+			removeClassFromElement(shape,"tempSelect");
+		}
+	}
 }
 
 function recursiveSelectShapes(sourceShape) {
-    if(doesElementHaveClass(sourceShape,"line")) {
-        recursiveSelectShapesDotStepper(sourceShape.dot1);
-        recursiveSelectShapesDotStepper(sourceShape.dot2);
-    } else {
-        var dots = sourceShape.dots;
-        for(var ii=0;ii<dots.length;ii++) {
-            recursiveSelectShapesDotStepper(dots[ii]);
-        }
-    }
+	if(doesElementHaveClass(sourceShape,"line")) {
+		recursiveSelectShapesDotStepper(sourceShape.dot1);
+		recursiveSelectShapesDotStepper(sourceShape.dot2);
+	} else {
+		var dots = sourceShape.dots;
+		for(var ii=0;ii<dots.length;ii++) {
+			recursiveSelectShapesDotStepper(dots[ii]);
+		}
+	}
 }
 
 function recursiveSelectShapesDotStepper(dot) {
-    var shape;
-    for(var ii=0;ii<dot.lines.length;ii++) {
-        shape = dot.lines[ii];
-        if(shape.isSelected()) {
-            continue;
-        } else {
-            shape.select();
-            recursiveSelectShapes(shape);
-        }
-    }
-    recursiveSelectShapesFacesOrImages(dot.faces);
-    recursiveSelectShapesFacesOrImages(dot.images);
-    recursiveSelectShapesFacesOrImages(dot.bases);
+	var shape;
+	for(var ii=0;ii<dot.lines.length;ii++) {
+		shape = dot.lines[ii];
+		if(shape.isSelected()) {
+			continue;
+		} else {
+			shape.select();
+			recursiveSelectShapes(shape);
+		}
+	}
+	recursiveSelectShapesFacesOrImages(dot.faces);
+	recursiveSelectShapesFacesOrImages(dot.images);
+	recursiveSelectShapesFacesOrImages(dot.bases);
 }
 
 function recursiveSelectShapesFacesOrImages(facesOrImages) {
-    var shape;
-    for(var jj=0;jj<facesOrImages.length;jj++) {
-        shape = facesOrImages[jj];
-        if(shape.isSelected()) {
-            continue;
-        } else {
-            shape.select();
-            recursiveSelectShapes(shape);
-        }
-    }
+	var shape;
+	for(var jj=0;jj<facesOrImages.length;jj++) {
+		shape = facesOrImages[jj];
+		if(shape.isSelected()) {
+			continue;
+		} else {
+			shape.select();
+			recursiveSelectShapes(shape);
+		}
+	}
 }
 
 function selectAllContiguousDots(sourceDot,event) {
-    var dots = getDots();
-    var dot;
-    if(!event.shiftKey) {
-        deselectAll();
-    } else {
-        for(var ii=0;ii<dots.length;ii++) {
-            dot = dots[ii];
-            if(dot.isSelected()) {
-                dot.deselect();
-                addClassToElement(dot,"tempSelect");
-            }
-        }
-    }
-    sourceDot.select();
-    recursiveSelectDot(sourceDot);
-    for(var ii=0;ii<dots.length;ii++) {
-        dot = dots[ii];
-        if(doesElementHaveClass(dot,"tempSelect")) {
-            dot.select();
-            removeClassFromElement(dot,"tempSelect");
-        }
-    }
+	var dots = getDots();
+	var dot;
+	if(!event.shiftKey) {
+		deselectAll();
+	} else {
+		for(var ii=0;ii<dots.length;ii++) {
+			dot = dots[ii];
+			if(dot.isSelected()) {
+				dot.deselect();
+				addClassToElement(dot,"tempSelect");
+			}
+		}
+	}
+	sourceDot.select();
+	recursiveSelectDot(sourceDot);
+	for(var ii=0;ii<dots.length;ii++) {
+		dot = dots[ii];
+		if(doesElementHaveClass(dot,"tempSelect")) {
+			dot.select();
+			removeClassFromElement(dot,"tempSelect");
+		}
+	}
 }
 
 function recursiveSelectDot(dot) {
-    var lines = dot.lines;
-    var line;
-    for(var ii=0;ii<lines.length;ii++) {
-        line = lines[ii];
-        if(line.dot1.isSelected() && line.dot2.isSelected()) {
-            continue;
-        }
-        if(line.dot1 == dot) {
-            line.dot2.select();
-            recursiveSelectDot(line.dot2);
-        } else if (line.dot2 == dot) {
-            line.dot1.select();
-            recursiveSelectDot(line.dot1);
-        }
-    }
-    recursiveSelectFacesOrImages(dot.faces,dot);
-    recursiveSelectFacesOrImages(dot.images,dot);
-    recursiveSelectFacesOrImages(dot.bases,dot);
+	var lines = dot.lines;
+	var line;
+	for(var ii=0;ii<lines.length;ii++) {
+		line = lines[ii];
+		if(line.dot1.isSelected() && line.dot2.isSelected()) {
+			continue;
+		}
+		if(line.dot1 == dot) {
+			line.dot2.select();
+			recursiveSelectDot(line.dot2);
+		} else if (line.dot2 == dot) {
+			line.dot1.select();
+			recursiveSelectDot(line.dot1);
+		}
+	}
+	recursiveSelectFacesOrImages(dot.faces,dot);
+	recursiveSelectFacesOrImages(dot.images,dot);
+	recursiveSelectFacesOrImages(dot.bases,dot);
 }
 
 function recursiveSelectFacesOrImages(facesOrImages,dot) {
-    var shape;
-    for(var ii=0;ii<facesOrImages.length;ii++) {
-        shape = facesOrImages[ii];
-        var allDone = true;
-        for(var jj=0;jj<shape.dots.length;jj++) {
-            if(!shape.dots[jj].isSelected()) {
-                allDone = false;
-            }
-        }
-        if(allDone) {
-            continue;
-        }
-        for(var jj=0;jj<shape.dots.length;jj++) {
-            if(shape.dots[jj] == dot) {
-                for(var kk=0;kk<shape.dots.length;kk++) {
-                    if(shape.dots[jj] != shape.dots[kk] && !shape.dots[kk].isSelected()) {
-                        shape.dots[kk].select();
-                        recursiveSelectDot(shape.dots[kk]);
-                    }
-                }
-            }
-        }
-    }
+	var shape;
+	for(var ii=0;ii<facesOrImages.length;ii++) {
+		shape = facesOrImages[ii];
+		var allDone = true;
+		for(var jj=0;jj<shape.dots.length;jj++) {
+			if(!shape.dots[jj].isSelected()) {
+				allDone = false;
+			}
+		}
+		if(allDone) {
+			continue;
+		}
+		for(var jj=0;jj<shape.dots.length;jj++) {
+			if(shape.dots[jj] == dot) {
+				for(var kk=0;kk<shape.dots.length;kk++) {
+					if(shape.dots[jj] != shape.dots[kk] && !shape.dots[kk].isSelected()) {
+						shape.dots[kk].select();
+						recursiveSelectDot(shape.dots[kk]);
+					}
+				}
+			}
+		}
+	}
 }
 
 function deselectAllDots() {
-    var dots = getDots();
-    for(var ii=0;ii<dots.length;ii++) {
-        dots[ii].deselect();
-    }
+	var dots = getDots();
+	for(var ii=0;ii<dots.length;ii++) {
+		dots[ii].deselect();
+	}
 }
 
 function deselectAll() {
-    deselectAllDots();
-    var lines=getLines();
-    for(var nn=0;nn<lines.length;nn++) {
-        lines[nn].deselect();
-    }
-    var faces=getFaces();
-    for(var ii=0;ii<faces.length;ii++) {
-        faces[ii].deselect();
-    }
-    var images=getImages();
-    for(var ii=0;ii<images.length;ii++) {
-        images[ii].deselect();
-    }
-    var bases=getBases();
-    for(var ii=0;ii<bases.length;ii++) {
-        bases[ii].deselect();
-    }
+	deselectAllDots();
+	var lines=getLines();
+	for(var nn=0;nn<lines.length;nn++) {
+		lines[nn].deselect();
+	}
+	var faces=getFaces();
+	for(var ii=0;ii<faces.length;ii++) {
+		faces[ii].deselect();
+	}
+	var images=getImages();
+	for(var ii=0;ii<images.length;ii++) {
+		images[ii].deselect();
+	}
+	var bases=getBases();
+	for(var ii=0;ii<bases.length;ii++) {
+		bases[ii].deselect();
+	}
 }
